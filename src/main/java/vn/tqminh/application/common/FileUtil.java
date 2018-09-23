a6570f5d13f20f8a1b8828f93cd652ae0f8b59a8
MinhTranQuang258 <finalleo258@gmail.com>
2018-09-23 15:21:46 +0700

    Update FileUtil

--- src/main/java/vn/tqminh/application/common/FileUtil.java
+++ src/main/java/vn/tqminh/application/common/FileUtil.java
@@ -3,9 +3,13 @@ package vn.tqminh.application.common;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.input.CharSequenceReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,9 +22,9 @@ public class FileUtil {
 
 	public static String readContent(String path) {
 		File file = new File(path);
-		String content= "";
+		String content = "";
 		try {
-			content= FileUtils.readFileToString(file, Charset.defaultCharset());
+			content = FileUtils.readFileToString(file, Charset.defaultCharset());
 			LOG.info("Reading a file from a path [{}]", path);
 		} catch (IOException e) {
 			LOG.error("Error while reading file's content with the path [{}]. Detail: {}", path, e);
@@ -28,6 +32,18 @@ public class FileUtil {
 		return content;
 	}
 	
+	public static List<String> readLine(String path) {
+		File file = new File(path);
+		List<String> content= new ArrayList<>();
+		try {
+			content = FileUtils.readLines(file, Charset.defaultCharset());
+			LOG.info("Reading lines from a path [{}]", path);
+		} catch (IOException e) {
+			LOG.error("Error while reading lines with the path [{}]. Detail: {}", path, e);
+		}
+		return content;
+	}
+
 	public static void wirteContent(String content, String path, CharSequence charSequence) {
 		File file = new File(path);
 		try {
@@ -37,9 +53,46 @@ public class FileUtil {
 			LOG.error("Error while writing file's content with the path [{}]. Detail: {}", path, e);
 		}
 	}
+
+	public static List<File> readFiles(String path, String[] extensions) {
+		File directory = new File(path);
+		List<File> files = (List<File>) FileUtils.listFiles(directory, extensions, true);
+		if(files != null) {
+			return files;
+		}
+		return new ArrayList<>();
+	}
+
+	public static List<String> readAndIgnoreContent(String path, String ignorePath) {
+		List<String> list= readLine(path);
+		Set<String> ignoreSet= new HashSet<>(readLine(ignorePath));
+		for (String element : list) {
+			if(ignoreSet.contains(element)) {
+				list.remove(element);
+			}
+			else {
+				continue;
+			}
+		}
+		return list;
+	}
 	
-	public static void readFileToMap() {
-		
+	public static List<String> readContentByList(String path, String providedPath) {
+		List<String> list= readLine(path);
+		Set<String> providedSet= new HashSet<>(readLine(providedPath));
+		for (String element : list) {
+			if(providedSet.contains(element)) {
+				continue;
+			}
+			else {
+				list.remove(element);
+			}
+		}
+		return list;
+	}
+
+	public static Map<String, String> readFileToMap(String path) {
+		return null;
 	}
 
 }
